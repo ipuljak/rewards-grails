1ac092938cfadf041d835b8a2dbba5375ed94e1d
Ivan <ivanpulj@gmail.com>
2017-04-12 23:43:54 -0400

    Added controller, domain, view

---
+++ grails-app/controllers/rewards/InventoryController.groovy
@@ -0,0 +1,21 @@
+package rewards
+
+class InventoryController {
+
+    // Index is considered the '/' page
+    def index() { 
+        render "Here is a list of everything."
+    }
+
+    def edit() {
+        def productName = "Breakfast Blend"
+        def sku = "BB01"
+        // This is a mapping - key/value pair sent to the view
+        [product:productName, sku:sku]
+    }
+
+    // '/remove' page
+    def remove() {
+        render "You have selected remove."
+    }
+}
