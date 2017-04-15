4866d54856fff0973de506471ae93bb03e55ee2b
Ivan <ivanpulj@gmail.com>
2017-04-15 01:46:58 -0400

    CRUD examples completed

--- grails-app/controllers/rewards/CustomerController.groovy
+++ grails-app/controllers/rewards/CustomerController.groovy
@@ -27,9 +27,57 @@ class CustomerController {
         [customerInstanceList:customerInstance]
     }
 
-    //def index() { }
-
     def checkin() {
         
+    }
+
+    // FOLLOWING OVERRIDES DYNAMIC SCAFFOLDING
+
+    // This sets up the Grails page view the same way it would automatically
+    // customerInstanceCount is needed for proper amount of pagination to do
+    def index() {
+        params.max = 10
+        [customerInstanceList: Customer.list(params), customerInstanceCount: Customer.count()]
+    }
+
+    // Create the customer form
+    def create() {
+        // Create a new instance of customer and pass all of the information from the params in
+        [customerInstance: new Customer()]
+    }
+
+    // Save the customer object from the form
+    // save will need a show
+    def save(Customer customerInstance) {
+        customerInstance.save()
+        // need to go into a "show" action after a save
+        redirect(action: "show", id: customerInstance.id)
+    }
+
+    def show(Long id) {
+        def customerInstance = Customer.get(id)
+        [customerInstance: customerInstance]
+    }
+
+    // edit will need an update
+    def edit(Long id) {
+        // query the item out of the database
+        def customerInstance = Customer.get(id)
+        [customerInstance: customerInstance]
+    }
+
+    // Working with two versions of the data - old and the edited
+    def update(Long id) {
+        def customerInstance = Customer.get(id)
+        customerInstance.properties = params
+        // use flush: true so that it executes it right away and persists
+        customerInstance.save(flush: true)
+        redirect(action: "show", id: customerInstance.id)
+    }
+
+    def delete(Long id) {
+        def customerInstance = Customer.get(id)
+        customerInstance.delete(flush: true)
+        redirect(action: "index")
     }
 }
